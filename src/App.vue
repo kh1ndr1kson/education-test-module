c42873a8999b013e739e6274bba3608ca1e272e1
kh1ndr1kson <kh1ndr.dev@gmail.com>
2021-09-08 12:03:56 +0300

    Set name component to shake-case

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app" class="p-grid">
-    <div class="p-col">
-      <MainContent />
-    </div>
-    <div class="p-col-fixed nav-panel-fixed">
-      <navigation-panel />
-    </div>
-  </div>
+	<div id="app" class="p-grid">
+		<div class="p-col">
+			<main-content />
+		</div>
+		<div class="p-col-fixed nav-panel-fixed">
+			<navigation-panel />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -18,19 +18,19 @@ import MainContent from "./components/MainContent.vue";
 import NavigationPanel from "./components/NavigationPanel.vue";
 
 export default {
-  name: "App",
-  components: {
-    MainContent,
-    NavigationPanel,
-  },
+	name: "App",
+	components: {
+		MainContent,
+		NavigationPanel,
+	},
 };
 </script>
 
 <style scoped>
 .nav-panel-fixed {
-  width: 300px;
-  height: 100%;
-  position: sticky;
-  top: 0;
+	width: 300px;
+	height: 100%;
+	position: sticky;
+	top: 0;
 }
 </style>
