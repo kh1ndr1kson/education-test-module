1070e14bd160917b4bed19e998d9dc55b7ab600d
kh1ndr1kson <kh1ndr.dev@gmail.com>
2021-09-10 12:00:20 +0300

    Update width for Nav panel

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app" class="p-grid">
-    <div class="p-col">
-      <main-content :items="compItems" />
-    </div>
-    <div class="p-col-fixed nav-panel-fixed">
-      <navigation-panel :items="compItems" />
-    </div>
-  </div>
+	<div id="app" class="p-grid">
+		<div class="p-col">
+			<main-content :items="compItems" />
+		</div>
+		<div class="p-col-fixed nav-panel-fixed">
+			<navigation-panel :items="compItems" />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -37,22 +37,22 @@ export default {
 		return {
 			isLoading,
 			items,
-			compItems
+			compItems,
 		};
 	},
 	name: "App",
 	components: {
 		MainContent,
 		NavigationPanel,
-	}
+	},
 };
 </script>
 
 <style scoped>
 .nav-panel-fixed {
-  width: 300px;
-  height: 100%;
-  position: sticky;
-  top: 0;
+	width: 400px;
+	height: 100%;
+	position: sticky;
+	top: 0;
 }
 </style>
