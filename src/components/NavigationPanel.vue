63a00535b4cc8fe54406b716dd8221d6226dc009
kh1ndr1kson <kh1ndr.dev@gmail.com>
2021-09-10 11:58:57 +0300

    Navigation panel set alternate align

--- src/components/NavigationPanel.vue
+++ src/components/NavigationPanel.vue
@@ -1,25 +1,25 @@
 <template>
-  <Timeline :value="items" align="right" class="customized-timeline">
-    <template #marker="slotProps">
-      <Avatar
-        :icon="
-          slotProps.item.checkeds.length > 0
-            ? `pi pi-check circle-check`
-            : `pi pi-times circle-times`
-        "
-        :style="
-          slotProps.item.checkeds.length > 0
-            ? `background-color: var(--green-500)`
-            : `background-color: var(--gray-300)`
-        "
-        size="small"
-        shape="circle"
-      />
-    </template>
-    <template #content="slotProps">
-      <small class="p-text-secondary">{{ slotProps.item.text }}</small>
-    </template>
-  </Timeline>
+	<Timeline :value="items" align="alternate" class="customized-timeline">
+		<template #marker="slotProps">
+			<Avatar
+				:icon="
+					slotProps.item.checkeds.length > 0
+						? `pi pi-check circle-check`
+						: `pi pi-times circle-times`
+				"
+				:style="
+					slotProps.item.checkeds.length > 0
+						? `background-color: var(--green-500)`
+						: `background-color: var(--gray-300)`
+				"
+				size="small"
+				shape="circle"
+			/>
+		</template>
+		<template #content="slotProps">
+			<small class="p-text-secondary">{{ slotProps.item.text }}</small>
+		</template>
+	</Timeline>
 </template>
 
 <script>
@@ -27,25 +27,24 @@ import Timeline from "primevue/timeline";
 import Avatar from "primevue/avatar";
 
 export default {
-	setup() {
-	},
+	setup() {},
 	name: "NavigationPanel",
 	components: {
 		Timeline,
 		Avatar,
 	},
 	props: {
-		items: Array
-	}
+		items: Array,
+	},
 };
 </script>
 
 <style>
 .circle-check {
-  color: var(--gray-100);
+	color: var(--gray-100);
 }
 
 .circle-times {
-  color: var(--gray-500);
+	color: var(--gray-500);
 }
 </style>
